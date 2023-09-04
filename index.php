39db74f80342e30775c7e14f8fd3ae8c790f11a8
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2023-09-04 12:33:21 +0200

    finalización de archivo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrando html con Php</title>
+
+</head>
+<body>
+
+<h1>Hola desde PHP usando Hrml</h1>
+
+<?php echo "Ahora esto esta impreso en PHP" ?>;
+
+</body>
+</html>
